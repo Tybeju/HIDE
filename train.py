f40f5e5fd471d1aa07002539e00e5256881ad335
Tybeju <151974866+Tybeju@users.noreply.github.com>
2024-05-21 21:05:28 +0200

    Update train.py
    class AverageMeter:
    self.sum += val -> self.sum += val * n

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ class AverageMeter:
 
     def update(self, val: float, n: int = 1) -> None:
         self.val = val
-        self.sum += val
+        self.sum += val*n
         self.count += n
         self.avg = self.sum / self.count
 
